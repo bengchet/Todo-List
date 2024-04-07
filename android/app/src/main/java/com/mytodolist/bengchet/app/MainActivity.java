48ab262234d50fe6b067cc3f6ef98214e0db5b6e
bengchet <popfibo@gmail.com>
2024-04-08 07:33:57 +0800

    Auto-generated commit

--- android/app/src/main/java/com/mytodolist/bengchet/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.mytodolist.bengchet.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- src/environment/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    supabaseUrl: 'https://ymjynsoyiehpkbepykwr.supabase.co',
-    supabaseKey: process.env['SUPABASE_KEY'] as string,
-    powersyncUrl: 'https://6610b9150835658c8e337553.powersync.journeyapps.com'
-}
